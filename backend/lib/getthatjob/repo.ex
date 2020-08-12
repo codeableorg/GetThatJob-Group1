66e6a8afa9e304fe93bf020395408e4024c993e2
acastemoreno <acastemoreno@gmail.com>
2020-08-12 16:59:16 -0500

    initial setup - code generated by helpers

---
+++ backend/lib/getthatjob/repo.ex
@@ -0,0 +1,5 @@
+defmodule Getthatjob.Repo do
+  use Ecto.Repo,
+    otp_app: :getthatjob,
+    adapter: Ecto.Adapters.Postgres
+end
---
+++ backend/test/test_helper.exs
@@ -0,0 +1,2 @@
+ExUnit.start()
+Ecto.Adapters.SQL.Sandbox.mode(Getthatjob.Repo, :manual)
