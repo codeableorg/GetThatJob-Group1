c7b00ef8c5c52560e86e89fe01dac473f8857753
acastemoreno <acastemoreno@gmail.com>
2020-08-13 13:20:41 -0500

    fix routes

--- backend/lib/getthatjob_web/router.ex
+++ backend/lib/getthatjob_web/router.ex
@@ -5,6 +5,17 @@ defmodule GetthatjobWeb.Router do
     plug :accepts, ["json"]
   end
 
+  scope "/" do
+    pipe_through :api
+
+    forward "/api", Absinthe.Plug, schema: GetthatjobWeb.Schema.Schema
+
+    forward "/graphiql", Absinthe.Plug.GraphiQL,
+      schema: GetthatjobWeb.Schema.Schema,
+      socket: GetthatjobWeb.UserSocket,
+      interface: :simple
+  end
+
   # Enables LiveDashboard only for development
   #
   # If you want to use the LiveDashboard in production, you should put
@@ -15,20 +26,9 @@ defmodule GetthatjobWeb.Router do
   if Mix.env() in [:dev, :test] do
     import Phoenix.LiveDashboard.Router
 
-    scope "/admin" do
+    scope "/" do
       pipe_through [:fetch_session, :protect_from_forgery]
       live_dashboard "/dashboard", metrics: GetthatjobWeb.Telemetry
     end
-  end
-
-  scope "/" do
-    pipe_through :api
-
-    forward "/api", Absinthe.Plug, schema: GetthatjobWeb.Schema.Schema
-
-    forward "/", Absinthe.Plug.GraphiQL,
-      schema: GetthatjobWeb.Schema.Schema,
-      socket: GetthatjobWeb.UserSocket,
-      interface: :simple
   end
 end
