a63a2955a25ba18b3806f468a5e4c586806ff337
acastemoreno <acastemoreno@gmail.com>
2020-08-14 16:09:43 -0500

    refactor graphql types in separate file

--- backend/lib/getthatjob_web/schema/schema.ex
+++ backend/lib/getthatjob_web/schema/schema.ex
@@ -3,9 +3,9 @@ defmodule GetthatjobWeb.Schema.Schema do
 
   alias Getthatjob.Recruitment
 
-  import Absinthe.Resolution.Helpers, only: [dataloader: 1]
-
   alias GetthatjobWeb.Resolvers
+
+  import_types(GetthatjobWeb.Schema.Types)
 
   query do
     @desc "Get a Job by title"
@@ -24,57 +24,6 @@ defmodule GetthatjobWeb.Schema.Schema do
 
       resolve(&Resolvers.Recruitment.jobs/3)
     end
-  end
-
-  input_object :job_filter do
-    field :matching, :string
-    field :country, :string
-    field :type, :string
-    field :salary_range, :salary_range
-    field :seniority, :string
-  end
-
-  input_object :salary_range do
-    field :low, non_null(:integer)
-    field :high, non_null(:integer)
-  end
-
-  enum :sort_order do
-    value(:asc)
-    value(:desc)
-  end
-
-  object :job do
-    field :id, non_null(:id)
-    field :title, non_null(:string)
-    field :type, non_null(:string)
-    field :seniority, non_null(:boolean)
-    field :salary, :integer
-    field :location, non_null(:string)
-    field :introduction, non_null(:string)
-    field :expected, non_null(:string)
-    field :looking_for, non_null(:string)
-    field :requirements, non_null(:string)
-    field :applications, list_of(:application), resolve: dataloader(Recruitment)
-  end
-
-  object :application do
-    field :id, non_null(:id)
-    field :cv_path, non_null(:string)
-    field :professional_experience, non_null(:string)
-    field :reason, non_null(:string)
-
-    field :professional, :professional, resolve: dataloader(Recruitment)
-  end
-
-  object :professional do
-    field :id, non_null(:id)
-    field :name, non_null(:string)
-    field :phone_number, non_null(:string)
-    field :description, non_null(:string)
-    field :experience, non_null(:string)
-    field :linkedin, :string
-    field :github, :string
   end
 
   def context(ctx) do
