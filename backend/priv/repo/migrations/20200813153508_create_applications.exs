a7e7841a809713678b1ff366609a0bea4391e871
acastemoreno <acastemoreno@gmail.com>
2020-08-14 15:56:29 -0500

    foreign keys not null

--- backend/priv/repo/migrations/20200813153508_create_applications.exs
+++ backend/priv/repo/migrations/20200813153508_create_applications.exs
@@ -6,8 +6,8 @@ defmodule Getthatjob.Repo.Migrations.CreateApplications do
       add :cv_path, :string, null: false
       add :professional_experience, :text, null: false
       add :reason, :text, null: false
-      add :job_id, references(:jobs, on_delete: :nothing)
-      add :professional_id, references(:professionals, on_delete: :nothing)
+      add :job_id, references(:jobs, on_delete: :nothing), null: false
+      add :professional_id, references(:professionals, on_delete: :nothing), null: false
 
       timestamps()
     end
