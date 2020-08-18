701b31b510bea989308b72967a7461479cf9c959
acastemoreno <acastemoreno@gmail.com>
2020-08-18 16:23:08 -0500

    mutation progress to signup

--- frontend/src/pages/SignupProfessional.js
+++ frontend/src/pages/SignupProfessional.js
@@ -1,6 +1,8 @@
 import React, { Fragment } from 'react';
 import { Formik } from 'formik';
 import * as Yup from 'yup';
+import { gql } from '@apollo/client';
+import { Mutation } from '@apollo/client/react/components';
 
 import {
   Title,
@@ -11,56 +13,84 @@ import {
 } from '../components/auth/StyledComponents';
 import TextInput from '../components/auth/TextInput';
 
+const SIGNUP_PROFESSIONAL_MUTATION = gql`
+  mutation SignupProfessional(
+    $email: String!
+    $password: String!
+    $passwordConfirmation: String!
+  ) {
+    signupProfessional(
+      user: {
+        email: $email
+        password: $password
+        passwordConfirmation: $passwordConfirmation
+      }
+    ) {
+      token
+      user {
+        type
+        email
+        id
+      }
+    }
+  }
+`;
+
 const SignupProfessional = () => {
   return (
     <Fragment>
       <Title>Sign Up</Title>
       <SubTitle>As Professional</SubTitle>
-      <Formik
-        initialValues={{
-          user: {
-            email: '',
-            password: '',
-            passwordConfirmation: '',
-          },
+      <Mutation mutation={SIGNUP_PROFESSIONAL_MUTATION}>
+        {(signup, { loading }) => {
+          return (
+            <Formik
+              initialValues={{
+                email: '',
+                password: '',
+                passwordConfirmation: '',
+              }}
+              validationSchema={Yup.object({
+                email: Yup.string()
+                  .email('Invalid email address')
+                  .required('Required'),
+                password: Yup.string()
+                  .max(20, 'Must be 20 characters or less')
+                  .required('Required'),
+                passwordConfirmation: Yup.string()
+                  .max(20, 'Must be 20 characters or less')
+                  .required('Required')
+                  .oneOf([Yup.ref('password')], 'Passwords must match'),
+              })}
+              onSubmit={(values, { setSubmitting }) => {
+                setTimeout(() => {
+                  signup({ variables: values });
+                  alert(JSON.stringify(values, null, 2));
+                  setSubmitting(false);
+                }, 400);
+              }}
+            >
+              <FormStyled>
+                <TextInput
+                  label="Email"
+                  name="email"
+                  type="email"
+                  placeholder="admin@mail.com"
+                />
+                <TextInput label="Password" name="password" type="password" />
+                <TextInput
+                  label="Password Confirmation"
+                  name="passwordConfirmation"
+                  type="password"
+                />
+                <SubmitStyled type="submit" disabled={loading}>
+                  Submit Up
+                </SubmitStyled>
+              </FormStyled>
+            </Formik>
+          );
         }}
-        validationSchema={Yup.object({
-          user: Yup.object({
-            email: Yup.string()
-              .email('Invalid email address')
-              .required('Required'),
-            password: Yup.string()
-              .max(20, 'Must be 20 characters or less')
-              .required('Required'),
-            passwordConfirmation: Yup.string()
-              .max(20, 'Must be 20 characters or less')
-              .required('Required')
-              .oneOf([Yup.ref('password')], 'Passwords must match'),
-          }),
-        })}
-        onSubmit={(values, { setSubmitting }) => {
-          setTimeout(() => {
-            alert(JSON.stringify(values, null, 2));
-            setSubmitting(false);
-          }, 400);
-        }}
-      >
-        <FormStyled>
-          <TextInput
-            label="Email"
-            name="user.email"
-            type="email"
-            placeholder="admin@mail.com"
-          />
-          <TextInput label="Password" name="user.password" type="password" />
-          <TextInput
-            label="Password Confirmation"
-            name="user.passwordConfirmation"
-            type="password"
-          />
-          <SubmitStyled type="submit">Submit Up</SubmitStyled>
-        </FormStyled>
-      </Formik>
+      </Mutation>
       <LinkStyled to="/sign-in">Sign in</LinkStyled>
     </Fragment>
   );
