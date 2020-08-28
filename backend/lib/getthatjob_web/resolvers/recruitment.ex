1df6a2b3cc9956c286e691034c0dfadcb6f35c0d
Ricardo Huamani <ricardohuamanip@gmail.com>
2020-08-28 11:19:19 -0500

    fix(backend): job schema arg from title to id

--- backend/lib/getthatjob_web/resolvers/recruitment.ex
+++ backend/lib/getthatjob_web/resolvers/recruitment.ex
@@ -1,8 +1,8 @@
 defmodule GetthatjobWeb.Resolvers.Recruitment do
   alias Getthatjob.Recruitment
 
-  def job(_, %{title: title}, _) do
-    {:ok, Recruitment.get_job_by_title!(title)}
+  def job(_, %{id: id}, _) do
+    {:ok, Recruitment.get_job!(id)}
   end
 
   def jobs(_, args, _) do
