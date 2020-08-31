9df775398d089c5e63e326d3e78b1f2319389190
acastemoreno <acastemoreno@gmail.com>
2020-08-30 20:22:47 -0500

    setting production

--- backend/config/prod.secret.exs
+++ backend/config/prod.secret.exs
@@ -12,9 +12,10 @@ database_url =
     """
 
 config :getthatjob, Getthatjob.Repo,
-  # ssl: true,
+  database: "",
+  ssl: true,
   url: database_url,
-  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10")
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "2")
 
 secret_key_base =
   System.get_env("SECRET_KEY_BASE") ||
